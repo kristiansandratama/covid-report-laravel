38de95f8753088cdbc9f017b37340f8c9665c2f1
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-08-26 12:43:35 +0700

    Add sum numbers to home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CovidCase;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $confirmed_sum = CovidCase::sum('confirmed');
+        $in_care_sum = CovidCase::sum('in_care');
+        $recovered_sum =  CovidCase::sum('recovered');
+        $dead_sum = CovidCase::sum('dead');
+
+        return view('home', [
+            'confirmed_sum' => $confirmed_sum,
+            'in_care_sum' => $in_care_sum,
+            'recovered_sum' => $recovered_sum,
+            'dead_sum' => $dead_sum,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,23 @@
 
 @section('content')
 <div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-        Home
+    <div class="grid grid-cols-2 gap-4 p-6 w-8/12">
+        <div class="p-4 text-center bg-white rounded-lg">
+            <p class="text-5xl p-2 text-yellow-400">{{ $confirmed_sum }}</p>
+            <p class="p-2">Confirmed</p>
+        </div>
+        <div class="p-4 text-center bg-white rounded-lg">
+            <p class="text-5xl p-2 text-yellow-800">{{ $in_care_sum }}</p>
+            <p class="p-2">In Care</p>
+        </div>
+        <div class="p-4 text-center bg-white rounded-lg">
+            <p class="text-5xl p-2 text-green-500">{{ $recovered_sum }}</p>
+            <p class="p-2">Recovered</p>
+        </div>
+        <div class="p-4 text-center bg-white rounded-lg">
+            <p class="text-5xl p-2 text-red-500">{{ $dead_sum }}</p>
+            <p class="p-2">Dead</p>
+        </div>
     </div>
 </div>
 @endsection
